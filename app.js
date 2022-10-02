d5b4f56e0fbaeb8e15fce956250289f7eeb2484a
austinProGit <lee_austin@columbusstate.edu>
2022-10-02 20:10:30 +0000

    integrated mongoDB

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
 
 app.use('/static', express.static('public'));
-app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
+app.set('view engine', 'ejs');
 
-app.get('/hello', function(req, res){
-    res.send('Hello world');
-})
+const mongoDB = 'mongodb+srv://lee_austin:IDngku4bcpIALVPC@cluster0.88oaz3q.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDb connection error: '))
 
 app.get('/', function(req, res){
     res.render('todo.ejs');
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body.content);
 })
 
 app.listen(3000, function(){
